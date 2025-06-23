79fe2d054a736196f1d80cefec82357271943c6f
Aldrin Joseph <yoaldrinjoseph@gmail.com>
2025-06-23 17:36:33 +0000

    When i ran lighthouse bench marck it only got a score of 69 for performa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-body',
+});
 
 export const metadata: Metadata = {
   title: 'Uni Chat',
@@ -14,12 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased h-full">
+      <head />
+      <body className={`font-body antialiased h-full ${inter.variable}`}>
         {children}
         <Toaster />
       </body>
